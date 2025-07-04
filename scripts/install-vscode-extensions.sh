8575524cf1ad189e99bdeff2a1dd30700daf28a6
Steven Ayvar <stevejettmedia@gmail.com>
2025-07-04 14:37:34 -0500

    adds vscode extension installation script

---
+++ scripts/install-vscode-extensions.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+extensions=(
+  ms-vscode-remote.remote-wsl
+  esbenp.prettier-vscode
+  dbaeumer.vscode-eslint
+  ritwickdey.LiveServer
+  formulahendry.auto-rename-tag
+  naumovs.color-highlight
+  eamodio.gitlens
+  donjayamanne.githistory
+  ecmel.vscode-html-css
+  zignd.html-css-class-completion
+  pranaygp.vscode-css-peek
+  ronnidc.nunjucks
+  phoenisx.cssvar
+  bchavez.vscode-eleventy
+  xabikos.javascriptsnippets
+  christian-kohler.path-intellisense
+  wayou.vscode-todo-highlight
+  gruntfuggly.todo-tree
+  alefragnani.project-manager
+  waderyan.rollupjs
+  eg2.vscode-npm-script
+  figma.figma-vscode-extension
+  kisstkondoros.vscode-gutter-preview
+  CoenraadS.bracket-pair-colorizer-2
+)
+
+for ext in "${extensions[@]}"; do
+  code --install-extension "$ext"
+done
+
+
